c7f47618ec7d7e15f821ebc4316a05a44790fc96
a-rusak <alexander.rusak@gmail.com>
2018-08-25 00:32:51 +0300

    Refactoring

--- src/scripts/actions-pager.js
+++ src/scripts/actions-pager.js
@@ -1,14 +1,16 @@
+'use strict'
+
 document.addEventListener('DOMContentLoaded', () => {
   const addPage = section => {
     let buffer = []
-    section.querySelectorAll('.card').forEach(item => {
-      if (item && item.offsetHeight !== 100) {
+    section.querySelectorAll('.actions-pager__card-holder').forEach(item => {
+      if (item && item.offsetHeight === 0) {
         buffer.push(item)
       }
     })
     if (buffer.length > 0) {
       const newSection = document.createElement('div')
-      newSection.classList.add('cards-pager__page')
+      newSection.classList.add('actions-pager__page')
       buffer.forEach(item => {
         newSection.appendChild(item)
       })
@@ -17,11 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  const wrapper = document.querySelector('.cards-pager__wrapper')
+  const wrapper = document.querySelector('.actions-pager__wrapper')
   if (wrapper === null) {
     return
   }
-  let section = wrapper.querySelector('.cards-pager__page')
+  let section = wrapper.querySelector('.actions-pager__page')
   const w = wrapper.offsetWidth
   section.style.width = w + 'px'
   // document.querySelector('.layout__main').style.width = w + 'px'
@@ -30,20 +32,20 @@ document.addEventListener('DOMContentLoaded', () => {
   //
 
   let currentPage = 0
-  const nextButton = document.querySelector('#next')
+  const nextButton = document.querySelector('#actions-page-next')
   nextButton.addEventListener('click', evt => {
     if (currentPage < wrapper.children.length - 1) {
       currentPage++
       wrapper.style.transform = `translate3d(-${wrapper.offsetWidth *
-        currentPage}px, 0, 0)`
+      currentPage}px, 0, 0)`
     }
   })
-  const prevButton = document.querySelector('#prev')
+  const prevButton = document.querySelector('#actions-page-prev')
   prevButton.addEventListener('click', evt => {
     if (currentPage > 0) {
       currentPage--
       wrapper.style.transform = `translate3d(-${wrapper.offsetWidth *
-        currentPage}px, 0, 0)`
+      currentPage}px, 0, 0)`
     }
   })
 })
